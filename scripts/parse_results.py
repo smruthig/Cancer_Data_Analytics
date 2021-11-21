107882c0a28d7f91b335203a7c85ed4bd3c7d5f9
yashi <yashichawla1@gmail.com>
2021-11-21 13:29:36 +0530

    all RF results

--- scripts/parse_results.py
+++ scripts/parse_results.py
@@ -3,18 +3,16 @@ import pandas as pd
 from pathlib import Path
 
 filenames = [Path(f) for f in Path(".").glob("*.json")]
-df = pd.DataFrame(columns=["Model", "Standardize",
-                  "Normalize", "PCA", "F1-Stratified", "F1", "Runtime"])
+df = pd.DataFrame(columns=["Model", "Standardize", "Normalize", "PCA", "F1-Stratified", "F1", "Runtime"])
 
 for filename in filenames:
-    print(filename)
     data = json.load(open(filename))
     model = filename.stem
     model_components = model.split("_", 1)
     model_name, namespace = model_components
     components = namespace[10:-1]
     components = components.split(",")
-
+    
     normalize = False
     standardize = False
     pca = False
@@ -32,12 +30,10 @@ for filename in filenames:
     runtime = data["runtime_mins"]
     if 'best_model_scores' in data:
         f1, f1_stratified = data['best_model_scores']
-    else:
+    else:    
         f1 = data["cv_scores_kfold"]
         f1_stratified = data["cv_scores_stratified"]
-    df.loc[len(df)] = [model_name, standardize,
-                       normalize, pca, f1_stratified, f1, runtime]
+    df.loc[len(df)] = [model_name, standardize, normalize, pca, f1_stratified, f1, runtime]
 
-df.sort_values(by=["F1-Stratified"], ascending=False,
-               inplace=True, ignore_index=True)
+df.sort_values(by=["F1-Stratified"], ascending=False, inplace=True, ignore_index=True)
 df.to_csv("model_results.csv", index=False)
